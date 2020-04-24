ac4cf823d2347ee5626320bd52f1a2a432d9eede
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-24 11:44:21 -0500

    Feat: connected jquery.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 
 const $ = require('jquery');
 
-const sayHello = () => { console.log('Hello') };
-sayHello();
+$(document).ready( () => {
+    console.log('dom is ready');
+    const sayHello = () => { console.log('Hello') };
+    sayHello();
+
+    const renderHello = (input) => { return `${sayHello()} there! My name is terminal from node. It's nice to meet you ${input}` };
+
+    let $textValue = $('#text-value');
+
+   $('#submit').click( (e) => {
+       e.preventDefault();
+
+       console.log($textValue.val());
+   });
+
+});
+
+
+
