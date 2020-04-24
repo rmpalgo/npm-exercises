63b43bfa41c9dea4111577a5c03a82a4ee5209d8
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-24 12:26:51 -0500

    Added webpack --watch, changed the main.js automatically.

--- src/index.js
+++ src/index.js
@@ -4,20 +4,19 @@ const $ = require('jquery');
 
 $(document).ready( () => {
     console.log('dom is ready');
-    const sayHello = () => { console.log('Hello') };
+    const sayHello = () => { return 'Hello' };
     sayHello();
 
-    const renderHello = (input) => { return `${sayHello()} there! My name is terminal from node. It's nice to meet you ${input}` };
+    const renderHello = () => { return `${sayHello()} there! My name is terminal from node. It's nice to meet you` };
 
-    ;
 
    $('#submit').click( (e) => {
        e.preventDefault();
        let $textValue = $('#text-value').val()
        console.log($textValue);
-       $('#render-text').append(`<li>${$textValue}</li>`);
+       $('#render-text').append(`<li>${renderHello()} ${$textValue}!</li>`);
    });
-
+    console.log('from node fropm node akdjfak;ldjfa;jdf;a');
 });
 
 
