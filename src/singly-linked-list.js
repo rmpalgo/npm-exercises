96038dc81dfa7be584d5c5d41795e0b2617008d7
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-08-05 21:34:02 -0500

    feat: implemented push or insertion in linked list, conditional if the head is null then set head as new node.

--- src/singly-linked-list.js
+++ src/singly-linked-list.js
@@ -20,10 +20,46 @@ class Node {
     }
 }
 
+/*
 let first = new Node("Hi");
 first.next = new Node("there");
 first.next.next = new Node("how");
 first.next.next.next = new Node("are");
 first.next.next.next.next = new Node("you?");
-
 console.log(first);
+ */
+
+
+/*
+when you maintain the tail pointer, the operation is only O(1)
+ */
+class SinglyLinkedList{
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if(this.head === null) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+}
+
+let list = new SinglyLinkedList();
+list.push("Hello");
+console.log(list);
+list.push("Goodbye");
+console.log(list);
+list.push("Hello Again");
+console.log(list);
+console.log(list.head.next.next);
